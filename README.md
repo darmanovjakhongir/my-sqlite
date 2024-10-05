ad486571c25caf3b15a2642b61ae6a63d5cb8850
JAKHONGIR DARMANOV <97786628+darmanovjakhongir@users.noreply.github.com>
2024-10-05 11:22:36 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+# My Sqlite
+
+A lightweight SQL-like command-line interface for working with CSV files as a database. MySQLite CLI supports 'SELECT', 'INSERT', 'UPDATE', 'DELETE', 'JOIN', 'WHERE' and 'ORDER' functions.
+
+## Contents
+
+- [Task](#task)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## Task
+
+- [x] You will need to create a folder and in this folder will be additional files containing your work.
+- [x] Folder names must start with special file names and also contain ( my_sqlite_request.rb my_sqlite_cli.rb ).
+- [x] Create a class called MySqliteRequest in my_sqlite_request.rb.
+- [x] It will have a similar behavior than a request on the real sqlite.
+
+## Description
+
+The MySQLite CLI allows users to perform SQL-like operations on CSV files. It is built using Ruby and provides a simple interface for database-like interactions. Supported commands include:
+
+- **SELECT**: select columns from a CSV file with optional filtering and sorting.
+- **INSERT**: Insert new lines into CSV file.
+- **UPDATE**: Update existing rows in a CSV file based on specified conditions.
+- **DELETE**: Delete rows from a CSV file based on specified conditions.
+- **JOIN**: Join two CSV files in specified columns.
+- **WHERE**: Filter rows based on conditions.
+- **Order**: sort rows based on column.
+
+## Installation
+
+1. Make sure you have Ruby installed on your system.
+2. Clone this repository: `https://github.com/darmanovjakhongir/my-sqlite.git`.
+3. Go to the project directory: `cd my-sqlite`.
+4. Install the required gems (CSV is part of the Ruby standard library): `bundle install`.
+5. Make sure the `my_sqlite_request.rb` file is in the same directory as your CLI script.
+
+## Usage
+
+1. Run the following command in the terminal to start My-SQLite: `ruby my_sqlite_cli.rb`.
+2. To exit My-SQLite, type exit and press Enter.
+
+## The Core Team
+
+<span><i> Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
+<span><img alt='Qwasar SV -- Software Engineering School Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px' /></span >
---
+++ my_sqlite_cli.rb
@@ -0,0 +1,102 @@
+require 'readline'
+require_relative "request"
+
+def read_cli_input
+  Readline.readline('my_sqlite_cli>> ', true)
+end
+
+def parse_key_value_pairs(arr)
+  result = {}
+  arr.each do |pair|
+    key, value = pair.split("=")
+    result[key.strip] = value.strip
+  end
+  result
+end
+
+def process_cli_request(action, args, request)
+  case action
+  when "from"
+    request.from(*args)
+  when "select"
+    request.select(args)
+  when "where"
+    column, value = args[0].split("=")
+    request.where(column.strip, value.strip)
+  when "insert"
+    request.insert(*args)
+  when "values"
+    request.values(parse_key_value_pairs(args))
+  when "update"
+    request.update(*args)
+  when "set"
+    request.set(parse_key_value_pairs(args))
+  when "delete"
+    request.delete
+  else
+    print_error_message(action)
+  end
+end
+
+def print_error_message(action = nil)
+  message = action ? "Undefined method: #{action}!" : "Undefined method!"
+  puts message
+end
+
+def parse_sql_input(sql)
+  valid_actions = ["SELECT", "FROM", "WHERE", "INSERT", "VALUES", "UPDATE", "SET", "DELETE"]
+  tokens = sql.split(" ")
+  command = nil
+  args = []
+  request = MySqliteRequest.new
+
+  tokens.each do |token|
+    if valid_actions.include?(token.upcase)
+      process_command(command, args, request) if command
+      command = token.downcase
+      args = []
+    else
+      args << token
+    end
+  end
+
+  finalize_request(command, args, request)
+end
+
+def process_command(command, args, request)
+  args = args.join(" ").split(", ")
+  process_cli_request(command, args, request)
+end
+
+def finalize_request(command, args, request)
+  if args.last&.end_with?(";")
+    args[-1] = args.last.chomp(";")
+    process_cli_request(command, args, request)
+    request.run
+  else
+    print_missing_semicolon_error
+  end
+end
+
+def print_missing_semicolon_error
+  puts "Finish your request with a semicolon (';')."
+end
+
+def run_cli
+  puts "Welcome to MySQLite CLI!"
+  puts "Type your SQL commands to interact with the database."
+  puts "End each command with a semicolon (';') and press Enter."
+  puts "Type 'exit' to quit the application."
+
+  while (command = read_cli_input)
+    break if command.strip.downcase == "exit"
+
+    begin
+      parse_sql_input(command)
+    rescue => e
+      puts "Error processing command: #{e.message}"
+    end
+  end
+end
+
+run_cli
